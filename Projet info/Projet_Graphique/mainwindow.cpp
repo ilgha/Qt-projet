9c6d501cbef7a8c0d552b7fe12fe3e6e7e293e62
ilgha <ilgha333@outlook.com>
2018-11-07 11:21:57 +0100

    oui

--- Projet info/Projet_Graphique/mainwindow.cpp
+++ Projet info/Projet_Graphique/mainwindow.cpp
@@ -47,7 +47,7 @@ void MainWindow::paintEvent(QPaintEvent *event){
         for(unsigned int i = 0; i<x; i++){
             QRectF target(i*width()/x, j*height()/y, width()/x, height()/y);
             QRectF source((t[j][i]-1)*16, 15, 16, 16);
-            QImage image("C:/Users/Gaspard/Desktop/github/Qt-projet/Projet info/advance wars sprites/tileset projet");
+            QImage image(":/sprt/advance wars sprites/tileset projet");
             QPainter painter(this);
             painter.drawImage(target, image, source);
         }
@@ -58,7 +58,7 @@ void MainWindow::paintEvent(QPaintEvent *event){
     for(unsigned int i = 0; i<army->size(); i++){
         QRectF target(( army->at(i)->getX())*width()/x, (army->at(i)->getY())*height()/y, width()/x, height()/y);
         QRectF source(0, 0, 16, 16);
-        QImage image("../advance wars sprites/player");
+        QImage image(":sprt/advance wars sprites/player");
         QPainter painter(this);
         painter.drawImage(target, image, source);
 
@@ -77,14 +77,14 @@ void MainWindow::paintEvent(QPaintEvent *event){
 
             QRectF source(56, 36, 16, 16);
             if(army->at(i)->getTeam() == game->getPlayer1()){
-                QImage image("C:/Users/Gaspard/Desktop/github/Qt-projet/Projet info/advance wars sprites/Orange_Star");
+                QImage image(":sprt/advance wars sprites/Orange_Star");
                 QPainter painter(this);
                 painter.drawImage(target, image, source);
                 painter.setPen(QPen(Qt::white));
                 painter.setFont(QFont("Times", 20, QFont::Bold));
                 painter.drawText(target, Qt::AlignBottom, QString::fromStdString(std::to_string(army->at(i)->getHealth())));
             }else {
-                QImage image("C:/Users/Gaspard/Desktop/github/Qt-projet/Projet info/advance wars sprites/Blue_Moon");
+                QImage image(":sprt/advance wars sprites/Blue_Moon");
                 QPainter painter(this);
                 painter.drawImage(target, image, source);
                 painter.setPen(QPen(Qt::white));
@@ -192,7 +192,7 @@ void MainWindow::showMove(Land l, Unit* unit){
 void MainWindow::showMenu(Building b, Unit u){
     QRectF target(11*this->width()/18, this->height()/12, this->width()/5,this->height()/3);
     QRectF source(0,0,41,62);
-    QImage image("C:/Users/Gaspard/Desktop/github/Qt-projet/Projet info/advance wars sprites/menu");
+    QImage image(":sprt/advance wars sprites/menu");
     QPainter painter(this);
     painter.drawImage(target, image, source);
 
