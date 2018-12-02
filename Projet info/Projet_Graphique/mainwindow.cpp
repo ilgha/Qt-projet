0055890f4c145dc17c9c4118106880ad0e251d68
Pierre Vander Eyken <Pierre Vander Eyken@LAPTOP-UTL3CACH>
2018-12-02 13:30:47 +0100

    dhfd

--- Projet info/Projet_Graphique/mainwindow.cpp
+++ Projet info/Projet_Graphique/mainwindow.cpp
@@ -16,6 +16,14 @@
 
 
 MainWindow::MainWindow(QWidget *parent, Game* game) : QMainWindow(parent), ui(new Ui::MainWindow){
+    QWidget window;
+       window.resize(320, 240);
+       window.setWindowTitle(QApplication::translate("childwidget", "Child widget"));
+       window.show();
+       QPushButton *button = new QPushButton(
+              QApplication::translate("childwidget", "Press me"), &window);
+          button->move(100, 100);
+          button->show();
     this->army[0] = army[0];
     this->game = game;
     ui->setupUi(this);
@@ -134,7 +142,7 @@ void MainWindow::sendJson(QJsonObject obj) {
 
 
 void MainWindow::paintEvent(QPaintEvent *event){
-
+/*
 
     //map
 
@@ -223,7 +231,7 @@ void MainWindow::paintEvent(QPaintEvent *event){
     painter.drawText(10, 250, QString("myTurn: ") + (myTurn ? "true" : "false"));
 
 
-    }
+    }*/
 }
 
 
