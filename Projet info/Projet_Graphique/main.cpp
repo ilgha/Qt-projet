ea21f2ef7ac42b6fd9260e2d200381db5bf11104
Pierre Vander Eyken <Pierre Vander Eyken@LAPTOP-UTL3CACH>
2018-12-16 16:15:43 +0100

    qtsdyf

--- Projet info/Projet_Graphique/main.cpp
+++ Projet info/Projet_Graphique/main.cpp
@@ -9,6 +9,7 @@ int main(int argc, char *argv[])
     Player player2 = Player(2, 100000);
     Game* game = new Game(&player1, &player2);
     MainWindow w(nullptr, game);
-    w.showMaximized();
+    //w.showMaximized();
+    w.show();
     return a.exec();
 }
